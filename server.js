95b74b316647aefa0624350e21200f7dfb25bf4b
Julien Bernard <julien@MacBook-Pro-de-Julien.local>
2025-06-22 17:16:01 +0200

    url site

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://tourneedesplages.fr/nospartenaires'
+}));
 app.use(express.json());
 
 // Configure SendGrid
